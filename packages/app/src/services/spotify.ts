0f2822afe62b7d6a92cf902acc82940cc157ba5d
emunhoz <edermunhozsantos@gmail.com>
2021-04-08 23:53:53 -0300

    chore: change url callback from spotify api redirect

--- packages/app/src/services/spotify.ts
+++ packages/app/src/services/spotify.ts
@@ -1,2 +1,6 @@
-const URL_REDIRECT = process.env.NODE_ENV === 'development' ? 'http://localhost:3000/#/' : process.env.REACT_APP_URL_PRODUCTION
-export const SPOTIFY_LOGIN_URL = `https://accounts.spotify.com/authorize?client_id=${process.env.REACT_APP_SPOTIFY_CLIENT_ID}&response_type=token&redirect_uri=${URL_REDIRECT}`;
+const URL_REDIRECT =
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:3000/#/callback'
+    : process.env.REACT_APP_URL_PRODUCTION
+
+export const SPOTIFY_LOGIN_URL = `https://accounts.spotify.com/authorize?client_id=${process.env.REACT_APP_SPOTIFY_CLIENT_ID}&response_type=token&redirect_uri=${URL_REDIRECT}`
