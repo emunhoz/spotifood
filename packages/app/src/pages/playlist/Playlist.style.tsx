d74fd52f5b8f806c739dadee3f6d7b199d49ec3d
emunhoz <edermunhozsantos@gmail.com>
2021-04-09 02:39:29 -0300

    chore: add layout mock filter with playlist card

--- packages/app/src/pages/playlist/Playlist.style.tsx
+++ packages/app/src/pages/playlist/Playlist.style.tsx
@@ -14,58 +14,20 @@ export const Logo = styled.div`
   margin-bottom: 20px;
 `
 
+export const SignOutLink = styled.span`
+  font-size: ${({ theme }) => theme.typography.size.s3}px;
+  color: ${({ theme }) => theme.palette.primary};
+
+  :hover {
+    cursor: pointer;
+  }
+`
+
 export const SpotifoodLogo = styled.img``
 
 export const Wrapper = styled.div``
 export const MainText = styled.div``
 export const ImageWrapper = styled.div``
-
-export const Title = styled.h1`
-  margin: 0;
-  margin-bottom: 10px;
-  font-size: ${({ theme }) => theme.typography.size.l1}px;
-  font-weight: ${({ theme }) => theme.typography.weight.bold};
-  color: ${({ theme }) => theme.palette.darkest};
-`
-
-export const Divider = styled.span`
-  display: inline-block;
-  width: 54px;
-  height: 4px;
-  margin-bottom: 10px;
-  background: ${({ theme }) => theme.palette.primary};
-  border-radius: ${({ theme }) => theme.attributes.borderRadius.big}px;
-`
-
-const TextStyle = css`
-  margin: 0;
-  font-size: ${({ theme }) => theme.typography.size.s4}px;
-  line-height: 36px;
-  color: ${({ theme }) => theme.palette.darkest};
-`
-
-export const Paragraph = styled.p`
-  ${TextStyle};
-`
-
-export const CallToAction = styled.div`
-  position: fixed;
-  right: 0;
-  bottom: 0;
-  left: 0;
-  display: flex;
-  flex-flow: column;
-  align-items: center;
-  padding: 26px;
-  text-align: center;
-  background: ${({ theme }) => theme.palette.lighter};
-  place-content: space-evenly;
-`
-
-export const ListenMusicImage = styled.img`
-  position: fixed;
-  bottom: 285px;
-`
 
 export const SpotifyLogo = styled.img`
   margin-bottom: 10px;
@@ -84,4 +46,52 @@ export const Filters = styled.div`
   font-weight: 500;
   text-transform: uppercase;
   letter-spacing: 0.75px;
+`
+
+export const PlayListWrapper = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(280px, 1fr));
+  grid-gap: 3em;
+  width: 100%;
+  padding: 40px 0;
+`
+
+const SlideToLeft = css`
+  right: 0;
+`
+
+export const CloseFilter = styled.div`
+  margin-bottom: 20px;
+`
+
+export const FilterWrapper = styled.div`
+  display: grid;
+  align-content: space-between;
+  max-width: 1280px;
+  height: -webkit-fill-available;
+  padding: 26px;
+  margin: 0 auto;
+`
+
+export const FilterContent = styled.div<{ toogleFilter?: boolean }>`
+  position: fixed;
+  top: 0;
+  right: -100%;
+  display: block;
+  width: 100%;
+  height: 100%;
+  background: #ffffffe8;
+  transition: 0.4s;
+
+  select,
+  input {
+    width: -webkit-fill-available;
+    padding: 18px;
+    margin-bottom: 20px;
+    background: #fff;
+    border: 1px solid #5a5a5a;
+    box-shadow: none;
+  }
+
+  ${({ toogleFilter }) => toogleFilter && SlideToLeft}
 `
--- packages/app/src/pages/playlist/Playlist.tsx
+++ packages/app/src/pages/playlist/Playlist.tsx
@@ -1,9 +1,11 @@
-import { SearchBar } from '@monorepo/ui-components'
+import { useState } from 'react'
+import { SearchBar, PlaylistCard, Button } from '@monorepo/ui-components'
 import SpotifoodLogo from '../../images/spotifood-logo.svg'
 import { useAuth } from '../../contexts/auth'
 import * as S from './Playlist.style'
 
 function Playlist () {
+  const [toggleFilter, setToogleFilter] = useState(false)
   const { signOut } = useAuth()
 
   return (
@@ -15,15 +17,88 @@ function Playlist () {
           width='124'
           height='45'
         />
-        <span onClick={() => signOut()}>Sair</span>
+        <S.SignOutLink onClick={() => signOut()}>Sair</S.SignOutLink>
       </S.Logo>
       <S.Wrapper>
         <S.SearchWrapper>
           <SearchBar onChange={console.log} placeholder='Nome da playlist...' />
           <div>
-            <S.Filters>Filtros</S.Filters>
+            <S.Filters onClick={() => setToogleFilter(!toggleFilter)}>
+              Filtros
+            </S.Filters>
+            <S.FilterContent toogleFilter={toggleFilter}>
+              <S.FilterWrapper>
+                <S.CloseFilter onClick={() => setToogleFilter(!toggleFilter)}>
+                  Fechar
+                </S.CloseFilter>
+                <div>
+                  <select>
+                    <option value=''>Locale</option>
+                    <option value=''>Teste</option>
+                    <option value=''>Teste</option>
+                  </select>
+
+                  <select>
+                    <option value=''>País</option>
+                    <option value=''>Teste</option>
+                    <option value=''>Teste</option>
+                  </select>
+
+                  <input
+                    type='datetime-local'
+                    id='meeting-time'
+                    name='meeting-time'
+                    value='2018-06-12T19:30'
+                    min='2018-06-07T00:00'
+                    max='2018-06-14T00:00'
+                  />
+
+                  <input type='text' value='20' />
+                  <input type='text' value='0' />
+                </div>
+                <Button>Aplicar</Button>
+              </S.FilterWrapper>
+            </S.FilterContent>
           </div>
         </S.SearchWrapper>
+        <S.PlayListWrapper>
+          <PlaylistCard
+            owner='Spotify'
+            playlistName='Rain Sounds'
+            link='https://open.spotify.com/playlist/37i9dQZF1DX8ymr6UES7vc'
+            imgUrl='https://i.scdn.co/image/ab67706f00000003aba1f07094bd3e98cd0122de'
+          />
+          <PlaylistCard
+            owner='Spotify'
+            playlistName='Rain Sounds'
+            link='https://open.spotify.com/playlist/37i9dQZF1DX8ymr6UES7vc'
+            imgUrl='https://i.scdn.co/image/ab67706f00000003aba1f07094bd3e98cd0122de'
+          />
+          <PlaylistCard
+            owner='Spotify'
+            playlistName='Rain Sounds'
+            link='https://open.spotify.com/playlist/37i9dQZF1DX8ymr6UES7vc'
+            imgUrl='https://i.scdn.co/image/ab67706f00000003aba1f07094bd3e98cd0122de'
+          />
+          <PlaylistCard
+            owner='Spotify'
+            playlistName='Rain Sounds'
+            link='https://open.spotify.com/playlist/37i9dQZF1DX8ymr6UES7vc'
+            imgUrl='https://i.scdn.co/image/ab67706f00000003aba1f07094bd3e98cd0122de'
+          />
+          <PlaylistCard
+            owner='Spotify'
+            playlistName='Rain Sounds'
+            link='https://open.spotify.com/playlist/37i9dQZF1DX8ymr6UES7vc'
+            imgUrl='https://i.scdn.co/image/ab67706f00000003aba1f07094bd3e98cd0122de'
+          />
+          <PlaylistCard
+            owner='Spotify'
+            playlistName='Rain Sounds'
+            link='https://open.spotify.com/playlist/37i9dQZF1DX8ymr6UES7vc'
+            imgUrl='https://i.scdn.co/image/ab67706f00000003aba1f07094bd3e98cd0122de'
+          />
+        </S.PlayListWrapper>
       </S.Wrapper>
     </S.Main>
   )
