b83b271ed47453a9d5592d14ca7321e5dedc415b
emunhoz <edermunhozsantos@gmail.com>
2021-04-12 10:44:18 -0300

    chore: use https instead of http to get filters data

--- packages/app/src/services/filters.ts
+++ packages/app/src/services/filters.ts
@@ -1,4 +1,4 @@
 import axios from 'axios'
 
 export const filterData = () =>
-  axios.get('http://www.mocky.io/v2/5a25fade2e0000213aa90776')
+  axios.get('https://www.mocky.io/v2/5a25fade2e0000213aa90776')
