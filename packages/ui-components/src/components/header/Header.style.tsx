1e526a2676933018ff13fe09f29105abcd9515b1
emunhoz <edermunhozsantos@gmail.com>
2021-05-16 03:20:05 -0300

    refactor: use tag header to header component

--- packages/ui-components/src/components/header/Header.style.tsx
+++ packages/ui-components/src/components/header/Header.style.tsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components'
 
-export const Header = styled.div`
+export const Header = styled.header`
   display: flex;
   align-items: center;
   justify-content: space-between;
