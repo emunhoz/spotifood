84b25cd4646d2b19d83a515beef66a47543c5b61
emunhoz <edermunhozsantos@gmail.com>
2021-05-16 03:39:56 -0300

    refactor: add isValidating in useFetch function

--- packages/app/src/hooks/use-fetch.ts
+++ packages/app/src/hooks/use-fetch.ts
@@ -1,12 +1,10 @@
 import useSWR from 'swr'
 import HTTP_CLIENT from '../services/api'
 
+const fetcher = (url: string) => HTTP_CLIENT.get(url).then(res => res.data)
+
 export function useFetch<Data = any, Error = any>(url: string) {
-  const { data, error } = useSWR<Data, Error>(url, async url => {
-    const response = await HTTP_CLIENT.get(url)
+  const { data, error, isValidating } = useSWR<Data, Error>(url, fetcher)
 
-    return response.data
-  })
-
-  return { data, error }
+  return { data, error, isValidating }
 }
