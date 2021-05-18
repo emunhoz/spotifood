5f73ef9f6e2643fe60eb1525e1a15ec207da1633
emunhoz <edermunhozsantos@gmail.com>
2021-05-18 01:06:57 -0300

    chore: use font system

--- packages/app/src/styles/global.ts
+++ packages/app/src/styles/global.ts
@@ -4,7 +4,8 @@ const GlobalStyle = createGlobalStyle`
   body {
     min-height: 100%;
     margin: 0;
-    font-family: 'Helvetica Neue', 'Arial', sans-serif;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 
+      Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol';
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     font-size: 16px;
